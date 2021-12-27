2f5578a94db47ae11a78c01a29b3b855a9c0a8a9
silverttthin <silverttthin@naver.com>
2021-12-27 15:04:44 +0900

    폴더 수정 및 주석 추가

--- 1316.py
+++ 1316.py
@@ -7,14 +7,15 @@ ans_cnt = 0
 
 def isGroupVoca(voca):
 
-    counter = dict(collections.Counter(voca))
+    counter = dict(collections.Counter(voca)) # 각 문자의 빈도 수 계산
 
     for char, freq in counter.items():
         first_idx : int  = voca.find(char) # 각 문자의 첫 인덱스를 찾는다.
         
         for i in range(1, freq):
-            if voca[first_idx + i] != char : return False
-        
+            if voca[first_idx + i] != char : return False 
+# 문자 수만큼 연속하여 나오지 않는다면 다른쪽에 있다는 의미이므로 False 리턴
+
     return True
 
 for case in range(n):
