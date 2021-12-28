305d8ac59cfd706c7a3a37f21140a9d79bbb2dce
leesiwoong <83443857+silverttthin@users.noreply.github.com>
2021-12-29 00:42:23 +0900

    Update 2869(timeout).py

--- 2869(timeout).py
+++ 2869(timeout).py
@@ -1,12 +1,11 @@
-import math
-h = 0
-a,b,v = 5,1,6
-day = math.ceil((v-b)/(a-b)) 
+#타임아웃 코드는 간단한 코드여서 생략, 하단은 정답코드
+import sys
 
-'''
-(a-b)*n + a = v, 
+A, B, V = map(int,sys.stdin.readline().split())
 
-n =(v-a)/(a-b)
-'''
-print(day)
-    
+# (A-B) * n+ A = V,  n= (V-A)/(A-B)
+
+if (V-A)%(A-B) == 0:
+    print(int((V-A)/(A-B))+1) # 
+else:
+    print(int((V-A)/(A-B))+2) # 원래는 올림 후 +1인데 올림처리 안했으므로 +2
